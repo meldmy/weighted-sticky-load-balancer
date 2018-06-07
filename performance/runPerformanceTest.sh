378a9772f5ae7db0964812fb734715528e290932
Dmytro Melnychuk <melnichukdv@gmail.com>
2018-06-07 18:55:43 +0200

    Add output for test folder as a parameter to run script

--- performance/runPerformanceTest.sh
+++ performance/runPerformanceTest.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 timestamp=$(date +%Y%m%d_%H%M%S)
+outputForTest=$1
 #TODO:// change container to newer JMeter version that will support JDK-10
 docker run \
-    --rm
+    --rm \
     --network="host" \
 	--name jmeter_performance \
-	--mount source=$1,target=/jmeter \
+	-v $outputForTest:/jmeter \
 	-t droplr/jmeter \
 	-n \
 	-t "PerformanceTestPlan.jmx" \
-	-j ./result/jmeter_$timestamp \
-	-l ./result/result_$timestamp.jtl \
+	-j $outputForTest/result/jmeter_$timestamp \
+	-l $outputForTest/result/result_$timestamp.jtl \
 	-e \
 	-o report__$(date +%Y%m%d_%H%M%S)/
 
